4b3f14df8fbaaa742f4065426b2bd763caf1397b
y1s2w3 <13186031563@163.com>
2021-05-25 14:18:45 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/ysw/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.ysw.git;
+
+/**
+ * @author ysw
+ * @create 2021-05-25 14:09
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
